a85013ee8e58f34a92fcbc81de133d9b7992380d
Cheikh Dieye <email>
2026-02-09 17:03:53 +0100

    Chapitre du post-symfony

---
+++ src/Controller/Admin/Post/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Admin\Post;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin')]
+final class PostController extends AbstractController
+{
+    #[Route('/post/index', name: 'app_admin_post_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('pages/admin/post/index.html.twig');
+    }
+
+    #[Route('/post/create', name: 'app_admin_post_create', methods: ['GET'])]
+    public function create(): Response
+    {
+        return $this->render('pages/admin/post/create.html.twig');
+    }
+}
